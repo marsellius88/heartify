660e1e3fc318c4b4817f48c3e76fc57fbe452c6d
marsellius88 <marselliustan1@gmail.com>
2024-12-16 19:36:43 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">This is a button</Button>
+    </div>
+  );
+}
+
+export default App;
